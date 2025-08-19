ce4ac4c261ed32e7d4e73080741755fd4649084b
Grain <2765966107@qq.com>
2025-08-19 17:22:13 +0800

    feat 4: commit networkio.cpp

--- 2.1/2.1.1/networkio.cpp
+++ 2.1/2.1.1/networkio.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <arpa/inet.h>
+#include <errno.h>
+
+using namespace std;
 int main()
 {
-    int sockfd  = socket (AF_INET ,SOCK_STREAM,0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_port = htons(2000);
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr)) == -1)
+    {
+        cout << "bind failed : " << strerror(errno) << endl;
+    }
+    listen(sockfd, 10);
+    cout << "listen end" << endl;
+
+    getchar();
+    cout << "exit" << endl;
     return 0;
 }
